b8e43585237555bdcb66996a537a5ca9042b68c9
Nancy Martin <nebulanancysironmk@gmail.com>
2026-04-09 13:41:40 +0530

    Day 1 and Day 2 - HelloWorld and VariablesDemo

---
+++ src/Week1/HelloWorld.java
@@ -0,0 +1,7 @@
+package Week1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Day 1. April 8. 120 days of discipline. From the worst days to the best days.");
+    }
+}
---
+++ src/Week1/VariablesDemo.java
@@ -0,0 +1,18 @@
+package Week1;
+
+public class VariablesDemo {
+    public static void main(String[] args){
+        String name = "Nancy";
+        int age = 22;
+        double marks = 98.5;
+        boolean isStudent = true;
+        char grade = 'A';
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Grade: " + grade);
+
+    }
+}
